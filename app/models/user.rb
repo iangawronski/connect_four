1714645bcb20be4125e5909f5ba6bfe78fcfb1e1
Ian Gawronski <iangawronski@gmail.com>
2015-02-12 14:27:11 -0500

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :user_games
+  has_many :games, through: :user_games
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def game
+  end
+
 end
+
+
