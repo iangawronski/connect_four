c18e846559fd5e046e300131cab0e527365cba1a
Matt Huhman <huhmanbeing@gmail.com>
2015-02-16 11:27:15 -0500

    Database updates, add players columns to game table

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213145326) do
+ActiveRecord::Schema.define(version: 20150216162016) do
 
   create_table "games", force: :cascade do |t|
     t.text     "board"
     t.integer  "turncount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "player1_id"
+    t.integer  "player2_id"
   end
 
   create_table "user_games", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "player1_id"
     t.integer  "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "player2_id"
   end
 
   create_table "users", force: :cascade do |t|
